3974e19d7ac000493b816d76e812dd34c2c58388
kurniahafizz11-commits <kurniahafizz11@gmail.com>
2026-04-16 23:31:22 +0700

    Update soal6.cpp

--- soal6.cpp
+++ soal6.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int n,total=0; 
+    //variabel total diinisialisai dengan 0 terlebih dahulu agar nilainya tidak acak (garbage value)
+
+    //meminta user menginput bilangan
+    cout<<"masukkan bilangan: ";
+    cin>>n;
+
+    //perulangan untuk mengambil semua bilangan genap dari 2 sampai n
+    //i+=2 agar langsung lompat ke bilangan genap selanjutnya
+    for(int i=2; i<=n; i+=2){
+        //menambahkan kuadrat dari bilangan genap ke variabel total
+        total+= i*i;
+    }
+
+    //menampilkan hasil total penjumlahan kuadrat bilangan genap
+    cout<<"total: "<<total;
+    return 0;
+}
