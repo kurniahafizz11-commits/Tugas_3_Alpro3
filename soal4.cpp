284e35364a910baada9f4adcfce087b4476f0e25
kurniahafizz11-commits <kurniahafizz11@gmail.com>
2026-04-16 23:51:15 +0700

    Update soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n; // n menyatakan jumlah pengulangan
+    cout << "Masukkan nilai n: "; // meminta user untuk menginputkan nilai n
+    cin >> n;
+
+    cout << "H"; // menampilkan huruf H
+
+    for (int i = 0; i < n; i++) // perulangan untuk menampilkan huruf 'o' sebanyak n kali
+    {
+        cout << "o";
+    }
+
+    cout << "r"; // menampilkan huruf 'r'
+
+    for (int i = 0; i < n; i++) // perulangan untuk menampilkan huruf 'e' sebanyak n kali
+    {
+        cout << "e";
+    }
+
+    for (int i = 0; i < n; i++) // perulangan untuk menampilkan tanda seru sebanyak n kali
+    {
+        cout << "!";
+    }
+    return 0;
+}
